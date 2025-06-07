44cdebcce64fa0f49a384d7b3e397d25f52902a4
notweuz <weuz.main@gmail.com>
2025-06-07 16:53:31 +0400

    Initial commit

---
+++ src/main/java/ru/ntwz/makemyfeed/MakeMyFeedApplication.java
@@ -0,0 +1,13 @@
+package ru.ntwz.makemyfeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeMyFeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakeMyFeedApplication.class, args);
+    }
+
+}
