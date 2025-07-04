6d7fe9ac982b07fe4f7acf84dd7ff00b353a694e
notweuz <weuz.main@gmail.com>
2025-07-05 01:18:57 +0400

    Add: contextLoad unit test

--- src/test/java/ru/ntwz/feedify/FeedifyApplcationTests.java
+++ src/test/java/ru/ntwz/feedify/FeedifyApplcationTests.java
@@ -1,8 +1,11 @@
 package ru.ntwz.feedify;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class FeedifyApplcationTests {
-    // TODO: make tests
+    @Test
+    void contextLoads() {
+    }
 }
