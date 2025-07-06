6689d3b06cb34a486dd0ddca539ddc4ef6f3e71e
notweuz <weuz.main@gmail.com>
2025-07-07 01:32:25 +0400

    Add ValidationConstant class for user input validation constants

---
+++ src/main/java/ru/ntwz/feedify/constant/ValidationConstant.java
@@ -0,0 +1,33 @@
+package ru.ntwz.feedify.constant;
+
+public class ValidationConstant {
+    public static class Length {
+        public static final int USERNAME_MIN = 3;
+        public static final int USERNAME_MAX = 20;
+
+        public static final int DISPLAY_NAME_MIN = 3;
+        public static final int DISPLAY_NAME_MAX = 50;
+
+        public static final int DESCRIPTION_MIN = 0;
+        public static final int DESCRIPTION_MAX = 200;
+
+        public static final int PASSWORD_MIN = 8;
+        public static final int PASSWORD_MAX = 100;
+    }
+
+    public static class Message {
+        public static final String INCORRECT_FORMAT = " is in incorrect format";
+
+        public static final String REQUIRED = " is required";
+
+        public static final String SHOULD_BE_POSITIVE_OR_ZERO = " should be positive or zero";
+
+        public static final String SHOULD_BE_POSITIVE = " should be positive";
+
+        public static final String WRONG_LENGTH = " should be between %d and %d characters";
+    }
+
+    public static class Regex {
+        public static final String NAME_REGEX = "^[A-Za-z0-9_-]+$";
+    }
+}
