46d52b3bf21ace9e67eff4bc9904a8f64914a3c3
notweuz <weuz.main@gmail.com>
2025-07-11 14:10:57 +0400

    Update 403 error handling in AuthorizationFilter

--- src/main/java/ru/ntwz/feedify/security/AuthorizationFilter.java
+++ src/main/java/ru/ntwz/feedify/security/AuthorizationFilter.java
@@ -1,11 +1,16 @@
 package ru.ntwz.feedify.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import io.micrometer.common.util.StringUtils;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContext;
@@ -15,21 +20,29 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import ru.ntwz.feedify.constant.AuthorizationConstant;
+import ru.ntwz.feedify.dto.response.ApiError;
 import ru.ntwz.feedify.exception.NotAuthorizedException;
 import ru.ntwz.feedify.service.JWTService;
 import ru.ntwz.feedify.service.implementation.CustomUserDetailsServiceImpl;
 
 import java.io.IOException;
+import java.util.Collections;
 
 @Component
+@Slf4j
 public class AuthorizationFilter extends OncePerRequestFilter {
     private final JWTService jwtService;
     private final CustomUserDetailsServiceImpl userDetailsService;
+    private final ObjectMapper mapper;
 
     @Autowired
     public AuthorizationFilter(JWTService jwtService, CustomUserDetailsServiceImpl userDetailsService) {
         this.jwtService = jwtService;
         this.userDetailsService = userDetailsService;
+
+        this.mapper = new ObjectMapper();
+        mapper.setPropertyNamingStrategy(PropertyNamingStrategies.SNAKE_CASE);
+        mapper.registerModule(new JavaTimeModule());
     }
 
     @Override
@@ -75,9 +88,19 @@ public class AuthorizationFilter extends OncePerRequestFilter {
             }
             filterChain.doFilter(request, response);
         } catch (NotAuthorizedException ex) {
+            ApiError apiError = new ApiError();
+            apiError.setErrors(Collections.singletonList(ex.toString()));
+            apiError.setReason("Not authorized to access this resource");
+            apiError.setStatus(HttpStatus.UNAUTHORIZED.name());
+            apiError.setMessage(ex.getMessage());
+
             response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
             response.setContentType("application/json");
-            response.getWriter().write("{\"error\": \"" + ex.getMessage() + "\"}");
+            try {
+                response.getWriter().write(mapper.writeValueAsString(apiError));
+            } catch (IOException e) {
+                log.error("Error writing response: {}", e.getMessage());
+            }
         }
     }
 }
